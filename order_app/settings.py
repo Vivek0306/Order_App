822a85ff4644a97b28f0fded4196dbc7891c4d24
Vivek Nair <vivekmanju53@gmail.com>
2023-02-27 22:19:10 +0530

    added static dir finders

--- order_app/settings.py
+++ order_app/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
