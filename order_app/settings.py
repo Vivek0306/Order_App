549d7af3666187a10fa429cb61f58ed159bc695b
Vivek Nair <vivekmanju53@gmail.com>
2023-03-07 00:23:22 +0530

    added orders

--- order_app/settings.py
+++ order_app/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
